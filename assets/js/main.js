b460f43266cb20a571549950c187034ae55ad130
peace <astonreba@gmail.com>
2018-12-03 12:11:45 +0300

    Modify file [Delivers #162236489]

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,18 +6,3 @@ function deleteOrder(){
     alert("You have deleted a parcel delivery order!");
 
 }
-/*function to verify user or admin*/
-function Verify(){
-    var username = document.querySelector("#username").value;
-    var password = document.querySelector("#password").value;
-    if(username==='user' && password ==="user"){
-       window.location.href ="vieworder.html";
-    }else if(username==='admin' && password ==="admin"){
-      var username = document.querySelector("#username").value;
-      var password = document.querySelector("#password").value;
-      window.location.href ="admin.html";
-  
-    }else{
-    }
-  }
-  
