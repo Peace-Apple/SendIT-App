3628bf0e8cbfe8d00c129560ddd1410e9ff14465
peace <astonreba@gmail.com>
2018-12-04 12:05:41 +0300

    Modify userview.js file [Delivers #162236561]

---
+++ assets/js/userview.js
@@ -0,0 +1,17 @@
+document.getElementById('orders').addEventListener('allOrders');
+
+function allOrders(){
+
+fetch('http://127.0.0.1:5000/api/v2/users/<int:user_id>/parcels/', {
+     method: 'GET',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type':'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+			'Access-Control-Allow-Origin': '*'
+
+        }
+})
+
+.then((res) => res.json)
+.then(())
