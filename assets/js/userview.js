3fa05aa4932282f33f80d2bcf96952f24883b689
peace <astonreba@gmail.com>
2018-12-06 11:00:59 +0300

    Modify file [Delivers #162236561]

--- assets/js/userview.js
+++ assets/js/userview.js
@@ -1,6 +1,6 @@
-document.getElementById('orders').addEventListener('allOrders');
+document.getElementById('orders').addEventListener('userOrders');
 
-function allOrders(){
+function userOrders(){
 
 fetch('http://127.0.0.1:5000/api/v2/users/<int:user_id>/parcels/', {
      method: 'GET',
