acb2c9b9943fd007c4e6ccac3c72fb337fb145ec
peace <astonreba@gmail.com>
2018-12-07 17:12:36 +0300

    Modify file

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -28,15 +28,15 @@ fetch('http://127.0.0.1:5000/api/v2/parcels/', {
                                     <label id="date"><strong>
                                     Posted At: ${response_object.data[i].order_date}</strong></label>
                                     &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <label><a href="">More Detail</a></label>
+                                    <label><button onclick = "detailedParcel(${response_object.data[i].parcel_id});"><a href="admindetail.html">More Detail</a></button></label>
                                     &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <label onclick="deleteOrder()"><a href="#">Delete</a></label>
+                                    <label><button onclick="deleteOrder()"><a href="#">Delete</a></button></label>
                                 </p>
 
                             </div>
                         </td>
                       </tr>`;
-            }
+            };
 
             document.getElementById('parcelOrders').innerHTML = output;
 
@@ -45,54 +45,53 @@ fetch('http://127.0.0.1:5000/api/v2/parcels/', {
         }
     });
 
+function detailedParcel(parcel_id){
+    let modal = document.getElementById('detail');
+    fetch(`http://127.0.0.1:5000/api/v2/parcels/${parcel_id}`, {
+     method: 'GET',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type':'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+			'Access-Control-Allow-Origin': '*'
+
+        }
+    })
+
+    .then((res) => res.json())
+    .then((response_object) => {
+        if (response_object.message === 'Successfully got one parcel delivery order'){
+            let parcel_data = response_object.data;
+            console.log(response_object);
+            console.log(response_object.data);
+
+            tab_data = '';
+            for(var i=0; i < response_object.data.length; i++){
+
+                tab_data += `<tr>
+                        <td><b>Senders Name:</b></td><td>${response_object.data[i].user_name}</td></tr>
+                        <tr><td><b>Senders Email:</b></td><td>${response_object.data[i].email}</td></tr>
+                        <tr><td><b>Senders Contact:</b></td><td>${response_object.data[i].phone_number}</td></tr>
+                        <tr><td><b>Receivers Name:</b></td><td>${response_object.data[i].receivers_name}</td></tr>
+                        <tr><td><b>Pickup Location:</b></td><td>${response_object.data[i].pickup_location}</td></tr>
+                        <tr><td><b>Destination:</b></td><td>${response_object.data[i].destination}</td></tr>
+                        <tr><td><b>Weight:</b></td><td>${response_object.data[i].weight}</td></tr>
+                        <tr><td><b>Order Date:</b></td><td>${response_object.data[i].order_date}</td></tr>
+                        <tr><td><b>Delivery Status:</b></td><td>${response_object.data[i].delivery_status}</td>
+                        <td class="links"><label onclick="editOrder()"><a href="#">Edit</a></label>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;</td></tr>
+                        <tr><td><b>Present Location:</b></td><td>${response_object.data[i].present_location}</td>
+                        <td class="links"><label onclick="editOrder()"><a href="#">Edit</a></label>
+                                                &nbsp;&nbsp;&nbsp;&nbsp;</td></tr>`;
 
 
-//function detailedParcel(parcel_id){
-//    let parcel_detail = document.getElementById('detail');
-//    fetch(`http://127.0.0.1:5000/api/v2/parcels/${parcel_id}`, {
-//     method: 'GET',
-//        headers: {
-//            'Accept': 'application/json, text/plain, */*',
-//            'Content-type':'application/json',
-//            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-//			'Access-Control-Allow-Origin': '*'
-//
-//        }
-//    })
-//
-//    .then((res) => res.json())
-//    .then((response_object) => {
-//        if (response_object.message === 'Successfully got one parcel delivery order'){
-//            let parcel_data = response_object.data;
-//            console.log(response_object);
-//            console.log(response_object.data);
-//
-//            table_data = '';
-//            for(var i=0; i < response_object.data.length; i++){
-//
-//                table_data += "<tr>"+
-//                        '<td><b>Senders Name:</b></td><td>'+response_object.data[i].user_name+'</td></tr>'+
-//                        '<tr><td><b>Senders Email:</b></td><td>'+response_object.data[i].email+'</td></tr>'+
-//                        '<tr><td><b>Senders Contact:</b></td><td>'+response_object.data[i].phone_number+'</td></tr>'+
-//                        '<tr><td><b>Receivers Name:</b></td><td>'+response_object.data[i].receivers_name+'</td></tr>'+
-//                        '<tr><td><b>Pickup Location:</b></td><td>'+response_object.data[i].pickup_location+'</td></tr>'+
-//                        '<tr><td><b>Destination:</b></td><td>'+response_object.data[i].destination+'</td></tr>'+
-//                        '<tr><td><b>Weight:</b></td><td>'+response_object.data[i].weight+'</td></tr>'+
-//                        '<tr><td><b>Order Date:</b></td><td>'+response_object.data[i].order_date+'</td></tr>'+
-//                        '<tr><td><b>Delivery Status:</b></td><td>'+response_object.data[i].delivery_status+'</td>'+
-//                        '<td class="links"><label onclick="editOrder()"><a href="#">Edit</a></label>'+
-//                                                "&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>"+
-//                        '<tr><td><b>Present Location:</b></td><td>'+response_object.data[i].present_location+'</td>'+
-//                        '<td class="links"><label onclick="editOrder()"><a href="#">Edit</a></label>'+
-//                                                "&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>";
-//
-//
-//            document.getElementById('parcel_detail').innerHTML = table_data;
-//        }
-//    }
-//    });
-//    }
-//
+            document.getElementById('parcel_detail').innerHTML = tab_data;
+            modal.style.display = "block";
+        };
+    }
+    });
+    }
+
 
 
 
