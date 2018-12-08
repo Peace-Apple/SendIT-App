e3e6ac3b12cb220ba2cd10b34262a1bffa713700
peace <astonreba@gmail.com>
2018-12-08 12:36:11 +0300

    Make changes in file [Delivers #162236644]

--- assets/js/usersingle.js
+++ assets/js/usersingle.js
@@ -5,67 +5,69 @@ if(/detail.html/.test(window.location.href)){
   let parcel_id = url.searchParams.get("parcel")
     console.log(parcel_id);
 
-  fetch('http://127.0.0.1:5000/api/v2/parcels/'+parcel_id, {
-     method: 'GET',
-     headers: {
+    fetch('http://127.0.0.1:5000/api/v2/parcels/'+parcel_id, {
+    method: 'GET',
+    headers: {
         'Accept': 'application/json, text/plain, */*',
         'Content-type':'application/json',
         'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
         'Access-Control-Allow-Origin': '*'
 
-     }
+    }
     })
 
-.then((res) => res.json())
-.then((response_object) => {
+    .then((res) => res.json())
+    .then((response_object) => {
     if (response_object.message === 'Successfully got one parcel delivery order'){
-        let order_data = response_object.data;
         console.log(response_object);
         console.log(response_object.data);
+        console.log(response_object.data.parcel_id);
 
         let output = `
 
-                <tr id="parcels_head">
-                    <th>Senders Name</th>
-                    <th>Receivers Name</th>
-                    <th>Pickup Location</th>
-                    <th>Destination</th>
-                    <th>Weight</th>
-                    <th>Order Date</th>
-                    <th>Delivery Status</th>
-                    <th>Present Location</th>
-                </tr>
+            <tr id="parcels_head">
+                <th>ID</th>
+                <th>Senders Name</th>
+                <th>Receivers Name</th>
+                <th>Pickup Location</th>
+                <th>Destination</th>
+                <th>Weight</th>
+                <th>Order Date</th>
+                <th>Delivery Status</th>
+                <th>Present Location</th>
+            </tr>
 
-            `;
-
+        `;
             output += `
 
                 <tr id="parcels">
-                        <td>${response_object.data.user_name}</td>
-                        <td>${response_object.data.receivers_name}</td>
-                        <td>${response_object.data.pickup_location}</td>
-                        <td>${response_object.data.destination}
-                        <p class="links">
-                            <label><a href="#">Edit</a></label>
-                         </p>
-                        </td>
-                        <td>${response_object.data.weight}</td>
-                        <td>${response_object.data.order_date}</td>
-                        <td>${response_object.data.delivery_status}
-                        <p class="links">
-                            <label onclick="cancelParcel()"><a href="#">Cancel</a></label>
-                         </p>
-                        </td>
-                        <td>${response_object.data.present_location}</td>
-                  </td>
+                    <td>${response_object.data.parcel_id}</td>
+                    <td>${response_object.data.user_name}</td>
+                    <td>${response_object.data.receivers_name}</td>
+                    <td>${response_object.data.pickup_location}</td>
+                    <td>${response_object.data.destination}
+                    <p class="links">
+                        <label><a href="#">Edit</a></label>
+                     </p>
+                    </td>
+                    <td>${response_object.data.weight}</td>
+                    <td>${response_object.data.order_date}</td>
+                    <td>${response_object.data.delivery_status}
+                    <p class="links">
+                        <label><a href="#">Cancel</a></label>
+                     </p>
+                    </td>
+                    <td>${response_object.data.present_location}</td>
+              </td>
 
-                </tr>`;
-        }
+            </tr>`;
 
-        document.getElementById('parcel_detail').innerHTML = output;
+        document.getElementById('parcelDetail').innerHTML = output;
 
     } else {
         console.log(response_object.message);
     }
     });
-}
+  }
+
+
