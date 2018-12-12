0ba856cfbba7cbfd57febd879db7d618a498a110
peace <astonreba@gmail.com>
2018-12-12 22:48:20 +0300

    Make changes in file [Delivers #162236561]

--- assets/js/userview.js
+++ assets/js/userview.js
@@ -1,17 +1,18 @@
+if(/vieworder.html/.test(window.location.href)){
 var payload = JSON.parse(atob(localStorage.getItem('accessToken').split('.')[1]));
 console.log(payload.identity[0]);
 console.log(payload)
 var user_id = payload.identity[0];
 
 fetch(`http://127.0.0.1:5000/api/v2/users/${user_id}/parcels/`, {
- method: 'GET',
+method: 'GET',
     headers: {
         'Accept': 'application/json, text/plain, */*',
         'Content-type':'application/json',
         'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-        'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*'
 
-    }
+}
 })
 
 .then((res) => res.json())
@@ -43,6 +44,9 @@ fetch(`http://127.0.0.1:5000/api/v2/users/${user_id}/parcels/`, {
                     <th>Order Date</th>
                     <th>Delivery Status</th>
                     <th>Present Location</th>
+                    <th>Edit Destination</th>
+                    <th>Cancel Delivery</th>
+                    <th>View Detail</th>
                 </tr>
 
             `;
@@ -54,19 +58,24 @@ fetch(`http://127.0.0.1:5000/api/v2/users/${user_id}/parcels/`, {
                         <td>${response_object.data[i].user_name}</td>
                         <td>${response_object.data[i].receivers_name}</td>
                         <td>${response_object.data[i].pickup_location}</td>
-                        <td>${response_object.data[i].destination}
-                        <p class="links">
-                            <label><a href='detail.html?parcel=${response_object.data[i].parcel_id}'>Edit</a></label>
-                         </p>
-                        </td>
+                        <td id="destinationUpdate">${response_object.data[i].destination}</td>
                         <td>${response_object.data[i].weight}</td>
                         <td>${response_object.data[i].order_date}</td>
-                        <td>${response_object.data[i].delivery_status}
-                        <p class="links">
-                            <label><a href='detail.html?parcel=${response_object.data[i].parcel_id}'>Cancel</a></label>
-                         </p>
-                        </td>
+                        <td id="parcelCancel">${response_object.data[i].delivery_status}</td>
                         <td>${response_object.data[i].present_location}</td>
+                        <td>
+                        <p class="links">
+                        <label><button onclick = "updateDestination(${response_object.data[i].parcel_id});">Edit</button></label>
+                        </p>
+                        </td>
+                        <td>
+                        <p class="links">
+                        <label><button onclick = "cancelParcel(${response_object.data[i].parcel_id});">Cancel</button></label>
+                        </p>
+                        </td>
+                        <td><p class="links">
+                            <label><a href='detail.html?parcel=${response_object.data[i].parcel_id}'>View</a></label>
+                         </p></td>
                   </td>
 
                 </tr>`;
@@ -78,3 +87,64 @@ fetch(`http://127.0.0.1:5000/api/v2/users/${user_id}/parcels/`, {
         console.log(response_object.message);
     }
 });
+
+function updateDestination(parcel_id){
+    var new_destination = window.prompt("Enter new destination")
+
+    fetch(`http://127.0.0.1:5000/api/v2/parcels/${parcel_id}/destination/`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+        cache: 'no-cache',
+        body: JSON.stringify({
+            destination: new_destination
+        })
+
+    })
+    .then((res) => res.json())
+    .then((response_object) => {
+        console.log(response_object);
+        if(response_object.message === 'Destination has been updated successfully'){
+            document.getElementById('destinationUpdate').innerHTML = new_destination
+            alert(response_object.message)
+        }
+        else{
+            alert(response_object.error_message)
+        }
+
+    })
+}
+
+function cancelParcel(parcel_id){
+    var cancel_parcel = "cancelled"
+
+    fetch(`http://127.0.0.1:5000/api/v2/parcels/${parcel_id}/cancel/`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+        cache: 'no-cache',
+        body: JSON.stringify({
+            delivery_status: cancel_parcel
+        })
+
+    })
+    .then((res) => res.json())
+    .then((response_object) => {
+        console.log(response_object);
+        if(response_object.message === 'Parcel delivery order has been cancelled successfully'){
+            document.getElementById('parcelCancel').innerHTML = cancel_parcel
+            alert(response_object.message)
+        }
+        else{
+            alert(response_object.error_message)
+        }
+
+    })
+}
+}
